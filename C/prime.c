0eb96dad791a9553860a731680b3d24f3c0b8526
Captain Ezio <iamgojoof6eyes@gmail.com>
2024-09-14 16:07:33 +0530

    Thinking abt it

---
+++ C/prime.c
@@ -0,0 +1,11 @@
+/*
+Those number which only have two factors which are the number itself and 1 are called prime number.
+
+*/
+
+#include <stdio.h>
+
+int main(){
+	//to be continued
+	return 0;
+}
