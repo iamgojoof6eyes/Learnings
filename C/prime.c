6577482c000410a50dbc9f5132edf12436639ed7
Captain Ezio <iamgojoof6eyes@gmail.com>
2024-09-14 16:47:29 +0530

    Will add comments

--- C/prime.c
+++ C/prime.c
@@ -6,6 +6,19 @@ Those number which only have two factors which are the number itself and 1 are c
 #include <stdio.h>
 
 int main(){
-	//to be continued
+	int n, i = 2;
+	
+	printf("Enter your number ");
+	scanf("%d", &n);
+	
+	for(i; i*i <= n; i++){
+		if(n%i == 0){
+			printf("%d is not a prime number.", n);
+			return 0;
+		}
+	}
+	
+	printf("%d is a prime number.", n);
+	
 	return 0;
 }
