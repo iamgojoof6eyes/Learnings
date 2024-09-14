901e07ea12cb165de100092dd393d45deb36e64e
Captain D. Ezio <iamgojoof6eyes@gmail.com>
2024-09-14 10:51:56 +0530

    Update factorial.c

--- C/factorial.c
+++ C/factorial.c
@@ -1,3 +1,11 @@
+/*
+Factorial is a mathematical term where we multiply every integer smaller than or equal to the given number down to one. The "!" sign denotes it. For example, the factorial of 4 is written as 4! and equals to 4*3*2*1. 
+It's worth noting that the factorial of 0 and 1 is 1 and for any negative integer factorial is not a real number.
+
+I will write a code in C that will calculate the factorial of any given positive number.
+*/
+
+
 #include <stdio.h>
 
 /*
@@ -12,6 +20,9 @@ int main() {
     if (n == 1 || n == 0){
         printf("Factorial of %d is 1",n);
         return 0;
+    }
+    else if (n < 0){
+        printf("Factorial of a negative number is not real.");
     }
     else{
         for(int i = n; i >= 1; i--){
@@ -36,13 +47,14 @@ int main() {
     
     if (n == 1 || n == 0){
         printf("Factorial of %d is 1",n);
-        return 0;
+    }
+    else if (n < 0){
+        printf("Factorial of a negative number is not real.");
     }
     else{
         f = factorial(n); 
+        printf("Factorial of %d is %d", n, f);
     }
-    printf("Factorial of %d is %d", n, f);
-
     return 0;
 }
 
