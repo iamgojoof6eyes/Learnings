99257e46bb227651ed700c0cf4278796154fd745
Captain D. Ezio <iamgojoof6eyes@gmail.com>
2024-09-15 14:18:58 +0530

    Added comment

--- C/prime.c
+++ C/prime.c
@@ -1,5 +1,5 @@
 /*
-Those number which only have two factors which are the number itself and 1 are called prime number.
+Those natural numbers that only have two factors: the number itself and 1 are called prime numbers.
 
 */
 
@@ -10,7 +10,9 @@ int main(){
 	
 	printf("Enter your number ");
 	scanf("%d", &n);
-	
+
+	// will loop from 2 to the square root of the given number or the square of the iterating number (basic maths)
+	// Why till square root? Because a number has the lowest factor within its square root, for example, 15 its square root is 3.87 and 3 is a factor of 15 which is less than its square root, you can also loop it till n/2 if u want
 	for(i; i*i <= n; i++){
 		if(n%i == 0){
 			printf("%d is not a prime number.", n);
