d004ef30b08dbd578cfb5f6095deabe2c5e52f0b
Captain Ezio <iamgojoof6eyes@gmail.com>
2025-02-14 15:52:39 +0530

    few adjustments

--- C/palindrome.c
+++ C/palindrome.c
@@ -4,6 +4,9 @@ Palindrome are those string or numbers which is equal even if you reverse them
 
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+
+void convert_lower(int, char*);
 
 int main() {
     char p[50]; //will update when learn more c
@@ -14,10 +17,10 @@ int main() {
     int j = strlen(p) - 1; //put a pointer at end of the string i.e. on last character
     
     int i = 0; //put a pointer at first character
-
+    convert_lower(j, p);
     while(i<j){
         if(p[i] != p[j]){
-            printf("Give string is not a palindrome\n");
+            printf("Give string %s is not a palindrome\n", p);
             return 0;
         }
         else{
@@ -26,7 +29,13 @@ int main() {
             continue;
         }
     }
-    printf("Given string is plaindrome\n");
+    printf("Given string %s is plaindrome\n", p);
 
     return 0;
+}
+
+void convert_lower(int len, char* string) {
+    for(int i = 0; i <= len; i++){
+        string[i] = tolower(string[i]);
+    }
 }
