b67c30964edd5c0dbfe4f3e6620bfe8d8c074acb
Captain Ezio <iamgojoof6eyes@gmail.com>
2025-03-23 13:46:42 +0530

    few additions

--- C/patterns.c
+++ C/patterns.c
@@ -13,15 +13,28 @@ void pattern3(int);
 
 void pattern4(int);
 
+void pattern5(int);
+
+int even(int, int);
+
+int odd(int, int);
+
+
 void main(){
     int lines;
 
     printf("How many lines should I print? ");
     scanf("%d", &lines);
 
-    pattern1w(lines);
+    pattern1(lines);
+
+    pattern2(lines);
+
+    pattern3(lines);
 
     pattern4(lines);
+
+    pattern5(lines);
 }
 
 void pattern1(int lines){
@@ -90,7 +103,7 @@ void pattern3(int lines){
 }
 
 
-void pattern4(int lines){
+void pattern4(int lines) {
     /*
     print:
         *
@@ -111,4 +124,47 @@ void pattern4(int lines){
         printf("\n");
     }
    printf("\n");
+}
+
+int even(int row, int n) {
+    int num = 1;
+    for(int i = 1; i<=n-row; i++){
+        num += 2;
+    }
+    return num;
+}
+
+int odd(int row, int col) {
+    return (row-col + 1)*2;
+}
+
+void pattern5(int n) {
+    /*
+    Pattern:
+    1
+    2 11
+    3 10 12
+    4 9 13 18
+    5 8 14 17 19
+    6 7 15 16 20 21
+    */
+    int start;
+
+    printf("Printing Pattern 5:\n");
+
+    for(int i = 1; i <= n; i++) {
+        start = i;
+        printf("%5d", i);
+        for(int j = 2; j <= i; j++) {
+            if(j%2) {
+                start += odd(i, j);
+            }
+            else {
+                start += even(i, n);
+            }
+            printf("%5d", start);
+        }
+        printf("\n");
+    }
+    printf("\n");
 }
